3404836567c14889b3c910a6d2f6858312d785cd
Daniele Oddo <daniele.oddo@outlook.com>
2021-10-17 14:47:40 +0200

    First Application of Login

---
+++ login/src/main/java/com/demo/srv/login/test/HelloController.java
@@ -0,0 +1,13 @@
+package com.demo.srv.login.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello Docker World";
+    }
+}
